202cb6cf6625c9f3db8b6d896c8c0e22174334d7
Julian Kropff <j.kropff@stud.unibas.ch>
2026-05-27 14:41:29 +0200

    fix

--- .config/nvim/lua/lsp/yaml.lua
+++ .config/nvim/lua/lsp/yaml.lua
@@ -2,40 +2,30 @@
 
 local M = {}
 
--- Wir verpacken das Setup in einen Autocommand für den Filetype 'yaml'
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "yaml",
-    callback = function()
-        -- Falls der Server für diesen Buffer schon läuft, brechen wir ab
-        local lspconfig = require('lspconfig')
-        
-        -- Standard-Capabilities für Autovervollständigung holen
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        local status_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-        if status_cmp then
-            capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-        end
+M.setup = function(capabilities)
+    local lspconfig = require("lspconfig")
 
-        lspconfig.yamlls.setup({
-            capabilities = capabilities,
-            settings = {
-                yaml = {
-                    -- Korrigierte URL: Das offizielle Compose-Spec Schema für fehlerfreie Autovervollständigung
-                    schemas = {
-                        ["https://githubusercontent.com"] = "docker-compose*.y*ml",
-                    },
+    lspconfig.yamlls.setup({
+        capabilities = capabilities,
+
+        filetypes = { "yaml", "yml" },
+
+        settings = {
+            yaml = {
+                schemas = {
+                    ["https://json.schemastore.org/docker-compose.json"] = "docker-compose*.y*ml",
                 },
             },
-            on_attach = function(client, bufnr)
-                local buf_map = function(mode, lhs, rhs, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, lhs, rhs, opts)
-                end
-                -- Hier kannst du bei Bedarf spezifische YAML-Keymaps eintragen
-            end,
-        })
-    end,
-})
+        },
+
+        on_attach = function(client, bufnr)
+            local buf_map = function(mode, lhs, rhs, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, lhs, rhs, opts)
+            end
+        end,
+    })
+end
 
 return M
