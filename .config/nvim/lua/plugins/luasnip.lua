9d524f190dd9b6710fb090c6989037f3698becba
Julian Kropff <j.kropff@stud.unibas.ch>
2026-05-28 21:45:47 +0200

    fix

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,5 +1,3 @@
--- plugin/luasnip.lua
-
 return {
   {
     "L3MON4D3/LuaSnip",
@@ -11,29 +9,13 @@ return {
     config = function()
       local ls = require("luasnip")
 
-      -- VSCode-like Standard Snippets (C, C++, Python, Rust, etc.)
+      -- VSCode Snippets (wichtig für blink + LSP + allgemeine Snippets)
       require("luasnip.loaders.from_vscode").lazy_load()
 
       ls.config.set_config({
         history = true,
         updateevents = "TextChanged,TextChangedI",
         enable_autosnippets = true,
-      })
-
-      local s = ls.snippet
-      local t = ls.text_node
-      local i = ls.insert_node
-
-      ls.add_snippets("cpp", {
-        s("class", {
-          t("class "),
-          i(1, "Name"),
-          t({ " {", "public:", "    " }),
-          i(2),
-          t({ "", "private:", "    " }),
-          i(3),
-          t({ "", "};" }),
-        }),
       })
     end,
   },
