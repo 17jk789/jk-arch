dfcdba13c88d408fb5fb7a4262042e1e10cb833c
Julian Kropff <j.kropff@stud.unibas.ch>
2026-06-30 22:35:43 +0200

    fix

--- .config/fish/functions/run-ghidra-scale.fish
+++ .config/fish/functions/run-ghidra-scale.fish
@@ -1,4 +1,4 @@
-function run-ghidra --description "Launch Ghidra (installed via yay) optimized for Hyprland/Wayland"
+function run-ghidra-scale --description "Launch Ghidra (installed via yay) optimized for Hyprland/Wayland"
     if not type -q ghidra
         echo (set_color red)"Error: Ghidra is not installed on the system!"(set_color normal)
         echo "Please install it using: yay -S ghidra"
