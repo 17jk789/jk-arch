50cdd474643f8897fcecf705c45b397d61f8fb85
Julian Kropff <j.kropff@stud.unibas.ch>
2026-07-02 13:23:49 +0200

    fix ghidra scale run arch config

--- .config/fish/functions/run-ghidra-scale.fish
+++ .config/fish/functions/run-ghidra-scale.fish
@@ -14,12 +14,16 @@ function run-ghidra-scale --description "Launch Ghidra (installed via yay) optim
     #                    -Dswing.aatext=true" \
     #     ghidra >/dev/null 2>&1 &
 
+    # env \
+    #     GDK_DPI_SCALE=1 \
+    #     _JAVA_OPTIONS="-Dsun.java2d.uiScale=2 \
+    #                    -Dawt.useSystemAAFontSettings=lcd \
+    #                    -Dswing.aatext=true \
+    #                    -Dsun.java2d.renderer=sun.java2d.marlin.MarlinRenderingEngine" \
+
     env \
         GDK_DPI_SCALE=1 \
-        _JAVA_OPTIONS="-Dsun.java2d.uiScale=2 \
-                       -Dawt.useSystemAAFontSettings=on \
-                       -Dswing.aatext=true \
-                       -Dsun.java2d.renderer=sun.java2d.marlin.MarlinRenderingEngine" \
+        _JAVA_OPTIONS="-Dsun.java2d.uiScale=2 -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.renderer=sun.java2d.marlin.MarlinRenderingEngine" \
         ghidra >/dev/null 2>&1 &
 
     disown
