d50719ec2f378928bdec8777e4633aa3b1097f7a
Julian Kropff <j.kropff@stud.unibas.ch>
2026-07-28 17:46:07 +0200

    fix(arch config): update gdb function with new flags and options

--- .config/fish/functions/gdb.fish
+++ .config/fish/functions/gdb.fish
@@ -1,29 +1,54 @@
 function gdb
-    set args $argv
+    set -l args $argv
 
-    set use_pwndbg 0
-    set use_gef 0
-    set use_all 0
-    set use_cat 0
+    set -l use_pwndbg 0
+    set -l use_gef 0
+    set -l use_all 0
+    set -l use_cat 0
+    set -l use_intel 0
+    set -l use_context_config 0
+    set -l stack_lines 30
+    set -l code_lines 25
+    set -l filtered_args
 
-    if contains -- -pwndbg $args
-        set use_pwndbg 1
-        set args (string match -rv -- '-pwndbg' $args)
-    end
+    for arg in $args
+        switch $arg
+            case '-pwndbg' '-pwngdb'
+                set use_pwndbg 1
 
-    if contains -- -gef $args
-        set use_gef 1
-        set args (string match -rv -- '-gef' $args)
-    end
+            case '-gef'
+                set use_gef 1
 
-    if contains -- -all $args
-        set use_all 1
-        set args (string match -rv -- '-all' $args)
-    end
+            case '-all'
+                set use_all 1
 
-    if contains -- -cat $args
-        set use_cat 1
-        set args (string match -rv -- '-cat' $args)
+            case '-cat'
+                set use_cat 1
+
+            case '-intel'
+                set use_intel 1
+
+            case '--stack-lines=*' '--stack_lines=*' '-stack-lines=*' '-stack_lines=*'
+                set use_context_config 1
+                set -l value (string replace -r -- '^--?stack[_-]?lines?=' '' $arg)
+                if test "$value" = "all"
+                    set stack_lines 999
+                else
+                    set stack_lines $value
+                end
+
+            case '--codelines=*' '--code_lines=*' '-codelines=*' '-code_lines=*' '-code-lines=*' '--code-lines=*'
+                set use_context_config 1
+                set -l value (string replace -r -- '^--?code[_-]?lines?=' '' $arg)
+                if test "$value" = "all"
+                    set code_lines 999
+                else
+                    set code_lines $value
+                end
+
+            case '*'
+                set filtered_args $filtered_args $arg
+        end
     end
 
     if test $use_gef -eq 1; and test $use_pwndbg -eq 1
@@ -31,23 +56,37 @@ function gdb
         return 1
     end
 
-    if test $use_cat -eq 1; and test $use_pwndbg -eq 0
-        echo "-cat requires -pwndbg"
+    if test $use_all -eq 1; and test $use_intel -eq 1
+        echo "Error: -all cannot be combined with -intel"
         return 1
     end
 
-    # GEF
+    if test $use_cat -eq 1; and test $use_gef -eq 1
+        echo "Error: -cat is only supported with -pwndbg"
+        return 1
+    end
+
+    if test $use_all -eq 1
+        set use_cat 1
+        set use_context_config 1
+    end
+
     if test $use_gef -eq 1
-        if test $use_all -eq 1
-            command gdb \
-                -iex "source /usr/share/gef/gef.py" \
+        set -l gef_cmds
+        set gef_cmds $gef_cmds -iex "source /usr/share/gef/gef.py"
+
+        if test $use_intel -eq 1
+            set gef_cmds $gef_cmds -iex "set disassembly-flavor intel"
+        end
+
+        if test $use_all -eq 1; or test $use_context_config -eq 1
+            set gef_cmds $gef_cmds \
                 -iex "gef config context.layout 'legend regs code args source memory stack threads trace extra dereference'" \
-                -iex "gef config context.nb_lines_code 25" \
-                -iex "gef config context.nb_lines_stack 25" \
+                -iex "gef config context.nb_lines_code $code_lines" \
+                -iex "gef config context.nb_lines_stack $stack_lines" \
                 -iex "gef config context.grow_stack_down True" \
                 -iex "gef config context.enable True" \
                 -iex "gef config context.clear_screen False" \
-                -iex "set disassembly-flavor intel" \
                 -iex "set pagination off" \
                 -iex "set breakpoint pending on" \
                 -iex "set print pretty on" \
@@ -55,20 +94,21 @@ function gdb
                 -iex "set follow-fork-mode child" \
                 -iex "set detach-on-fork off" \
                 -iex "set disassemble-next-line on" \
-                -iex "set confirm off" \
-                $args
-        else
-            command gdb \
-                -iex "source /usr/share/gef/gef.py" \
-                $args
+                -iex "set confirm off"
         end
 
-        # Pwndbg
+        command gdb $gef_cmds $filtered_args
+
     else if test $use_pwndbg -eq 1
-        if test $use_all -eq 1
-            command gdb \
-                -iex "source /usr/share/pwndbg/gdbinit.py" \
-                -iex "set disassembly-flavor intel" \
+        set -l pwndbg_cmds
+        set pwndbg_cmds $pwndbg_cmds -iex "source /usr/share/pwndbg/gdbinit.py"
+
+        if test $use_intel -eq 1
+            set pwndbg_cmds $pwndbg_cmds -iex "set disassembly-flavor intel"
+        end
+
+        if test $use_all -eq 1; or test $use_context_config -eq 1
+            set pwndbg_cmds $pwndbg_cmds \
                 -iex "set pagination off" \
                 -iex "set print pretty on" \
                 -iex "set print asm-demangle on" \
@@ -78,23 +118,18 @@ function gdb
                 -iex "set disassemble-next-line on" \
                 -iex "set history save on" \
                 -iex "set confirm off" \
-                -iex "set context-stack-lines 30" \
-                -iex "set context-code-lines 25" \
-                -iex "set context-clear-screen off" \
-                $args
-
-        else if test $use_cat -eq 1
-            command gdb \
-                -iex "source /usr/share/pwndbg/gdbinit.py" \
-                -iex "set syntax-highlight-style catppuccin_mocha" \
-                $args
-        else
-            command gdb \
-                -iex "source /usr/share/pwndbg/gdbinit.py" \
-                $args
+                -iex "set context-stack-lines $stack_lines" \
+                -iex "set context-code-lines $code_lines" \
+                -iex "set context-clear-screen off"
         end
 
+        if test $use_cat -eq 1
+            set pwndbg_cmds $pwndbg_cmds -iex "set syntax-highlight-style catppuccin_mocha"
+        end
+
+        command gdb $pwndbg_cmds $filtered_args
+
     else
-        command gdb $args
+        command gdb $filtered_args
     end
 end
