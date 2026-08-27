ffba2cf2ce97fb9c3206762bb3fd74f5976c9bff
JK <julian.kropff@icloud.com>
2026-08-27 12:27:18 +0200

    fix(arch config): update border-visible.fish to use regex for color replacement

--- .config/fish/functions/border-visible.fish
+++ .config/fish/functions/border-visible.fish
@@ -6,53 +6,49 @@ function border-visible
     if grep -q -- "-- BORDER_VISIBLE_ON" $general
 
         # ============================================================
-        # BORDER VISIBLE -> OFF / DEFAULT
+        # ON -> OFF / DEFAULT
         # ============================================================
 
-        # Status-Marker entfernen
         sed -i '/-- BORDER_VISIBLE_ON/d' $general
 
-        # general.lua -> DEFAULT
-        sed -i 's/border_size = 2,/border_size = 1,/' $general
+        # egal welche Farbe vorher war -> DEFAULT
+        sed -i -E 's/border_size = [0-9]+,/border_size = 1,/' $general
 
-        sed -i 's/colors = { "rgba(cba6f7FF)", "rgba(cba6f7FF)", "rgba(cba6f7FF)" }/colors = { "rgba(ffffff88)", "rgba(61D6FFAA)", "rgba(7BCBFFFF)" }/' $general
+        sed -i -E 's/colors = \{ "rgba\([^"]+\)", "rgba\([^"]+\)", "rgba\([^"]+\)" \}/colors = { "rgba(ffffff88)", "rgba(61D6FFAA)", "rgba(7BCBFFFF)" }/' $general
 
-        sed -i 's/colors = { "rgba(cba6f733)" }/colors = { "rgba(ffffff18)" }/' $general
+        sed -i -E 's/colors = \{ "rgba\([^"]+\)" \}/colors = { "rgba(ffffff18)" }/' $general
 
-        # colors.lua -> DEFAULT
-        sed -i 's/active_border = "rgba(cba6f7FF)"/active_border = "rgba(47464877)"/' $colors
+        # colors.lua -> DEFAULT egal was vorher drin war
+        sed -i -E 's/active_border = ".*"/active_border = "rgba(47464877)"/' $colors
 
-        sed -i 's/inactive_border = "rgba(cba6f733)"/inactive_border = "rgba(1c1b1d33)"/' $colors
+        sed -i -E 's/inactive_border = ".*"/inactive_border = "rgba(1c1b1d33)"/' $colors
 
-        sed -i 's/background_color = "rgba(1e1e2eFF)"/background_color = "rgba(131315FF)"/' $colors
+        sed -i -E 's/background_color = ".*"/background_color = "rgba(131315FF)"/' $colors
 
-        sed -i 's/border_color = "rgba(cba6f7AA) rgba(cba6f777)"/border_color = "rgba(c6c5d6AA) rgba(c6c5d677)"/' $colors
+        sed -i -E 's/border_color = ".*"/border_color = "rgba(c6c5d6AA) rgba(c6c5d677)"/' $colors
 
         notify-send "Border Visible" OFF
 
     else
 
         # ============================================================
-        # BORDER VISIBLE -> ON / LILA #cba6f7
+        # OFF -> ON / LILA
         # ============================================================
 
-        # general.lua -> LILA #cba6f7
-        sed -i 's/border_size = 1,/border_size = 2,/' $general
+        sed -i -E 's/border_size = [0-9]+,/border_size = 2,/' $general
 
-        sed -i 's/colors = { "rgba(ffffff88)", "rgba(61D6FFAA)", "rgba(7BCBFFFF)" }/colors = { "rgba(cba6f7FF)", "rgba(cba6f7FF)", "rgba(cba6f7FF)" }/' $general
+        sed -i -E 's/colors = \{ "rgba\([^"]+\)", "rgba\([^"]+\)", "rgba\([^"]+\)" \}/colors = { "rgba(cba6f7FF)", "rgba(cba6f7FF)", "rgba(cba6f7FF)" }/' $general
 
-        sed -i 's/colors = { "rgba(ffffff18)" }/colors = { "rgba(cba6f733)" }/' $general
+        sed -i -E 's/colors = \{ "rgba\([^"]+\)" \}/colors = { "rgba(cba6f733)" }/' $general
 
-        # colors.lua -> LILA #cba6f7
-        sed -i 's/active_border = "rgba(47464877)"/active_border = "rgba(cba6f7FF)"/' $colors
+        sed -i -E 's/active_border = ".*"/active_border = "rgba(cba6f7FF)"/' $colors
 
-        sed -i 's/inactive_border = "rgba(1c1b1d33)"/inactive_border = "rgba(cba6f733)"/' $colors
+        sed -i -E 's/inactive_border = ".*"/inactive_border = "rgba(cba6f733)"/' $colors
 
-        sed -i 's/background_color = "rgba(131315FF)"/background_color = "rgba(1e1e2eFF)"/' $colors
+        sed -i -E 's/background_color = ".*"/background_color = "rgba(1e1e2eFF)"/' $colors
 
-        sed -i 's/border_color = "rgba(c6c5d6AA) rgba(c6c5d677)"/border_color = "rgba(cba6f7AA) rgba(cba6f777)"/' $colors
+        sed -i -E 's/border_color = ".*"/border_color = "rgba(cba6f7AA) rgba(cba6f777)"/' $colors
 
-        # Status-Marker IMMER erst ganz am Ende setzen
         printf '%s\n' '-- BORDER_VISIBLE_ON' >>$general
 
         notify-send "Border Visible" ON
