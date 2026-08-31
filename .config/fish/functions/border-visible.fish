0b7833e6a4bb1579a5515165eae1d479e3328b83
Julian Kropff <j.kropff@stud.unibas.ch>
2026-08-31 16:38:44 +0200

    fix(arch config): update border-visible function for better color handling

--- .config/fish/functions/border-visible.fish
+++ .config/fish/functions/border-visible.fish
@@ -11,21 +11,21 @@ function border-visible
 
         sed -i '/-- BORDER_VISIBLE_ON/d' $general
 
-        # egal welche Farbe vorher war -> DEFAULT
-        sed -i -E 's/border_size = [0-9]+,/border_size = 1,/' $general
+        # Rahmenstärke auf Standard setzen
+        sed -i -E 's/border_size[[:space:]]*=[[:space:]]*[0-9]+,/border_size = 1,/' $general
 
-        sed -i -E 's/colors = \{ "rgba\([^"]+\)", "rgba\([^"]+\)", "rgba\([^"]+\)" \}/colors = { "rgba(ffffff88)", "rgba(61D6FFAA)", "rgba(7BCBFFFF)" }/' $general
+        # general.lua -> DEFAULT
+        # Aktiver Border
+        sed -i -E 's/(colors[[:space:]]*=[[:space:]]*\{[[:space:]]*)"rgba\([^"]+\)",[[:space:]]*"rgba\([^"]+\)",[[:space:]]*"rgba\([^"]+\)"([[:space:]]*\})/\1"rgba(ffffff88)", "rgba(61D6FFAA)", "rgba(7BCBFFFF)"\4/' $general
 
-        sed -i -E 's/colors = \{ "rgba\([^"]+\)" \}/colors = { "rgba(ffffff18)" }/' $general
+        # Inaktiver Border
+        sed -i -E 's/(colors[[:space:]]*=[[:space:]]*\{[[:space:]]*)"rgba\([^"]+\)"([[:space:]]*\})/\1"rgba(ffffff18)"\2/' $general
 
-        # colors.lua -> DEFAULT egal was vorher drin war
-        sed -i -E 's/active_border = ".*"/active_border = "rgba(47464877)"/' $colors
-
-        sed -i -E 's/inactive_border = ".*"/inactive_border = "rgba(1c1b1d33)"/' $colors
-
-        sed -i -E 's/background_color = ".*"/background_color = "rgba(131315FF)"/' $colors
-
-        sed -i -E 's/border_color = ".*"/border_color = "rgba(c6c5d6AA) rgba(c6c5d677)"/' $colors
+        # colors.lua -> DEFAULT
+        sed -i -E 's/(active_border[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(47464877)\2/' $colors
+        sed -i -E 's/(inactive_border[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(1c1b1d33)\2/' $colors
+        sed -i -E 's/(background_color[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(131315FF)\2/' $colors
+        sed -i -E 's/(border_color[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(c6c5d6AA) rgba(c6c5d677)\2/' $colors
 
         notify-send "Border Visible" OFF
 
@@ -35,19 +35,21 @@ function border-visible
         # OFF -> ON / LILA
         # ============================================================
 
-        sed -i -E 's/border_size = [0-9]+,/border_size = 2,/' $general
+        # Rahmenstärke erhöhen
+        sed -i -E 's/border_size[[:space:]]*=[[:space:]]*[0-9]+,/border_size = 2,/' $general
 
-        sed -i -E 's/colors = \{ "rgba\([^"]+\)", "rgba\([^"]+\)", "rgba\([^"]+\)" \}/colors = { "rgba(cba6f7FF)", "rgba(cba6f7FF)", "rgba(cba6f7FF)" }/' $general
+        # general.lua -> LILA
+        # Aktiver Border
+        sed -i -E 's/(colors[[:space:]]*=[[:space:]]*\{[[:space:]]*)"rgba\([^"]+\)",[[:space:]]*"rgba\([^"]+\)",[[:space:]]*"rgba\([^"]+\)"([[:space:]]*\})/\1"rgba(cba6f7FF)", "rgba(cba6f7FF)", "rgba(cba6f7FF)"\4/' $general
 
-        sed -i -E 's/colors = \{ "rgba\([^"]+\)" \}/colors = { "rgba(cba6f733)" }/' $general
+        # Inaktiver Border
+        sed -i -E 's/(colors[[:space:]]*=[[:space:]]*\{[[:space:]]*)"rgba\([^"]+\)"([[:space:]]*\})/\1"rgba(cba6f733)"\2/' $general
 
-        sed -i -E 's/active_border = ".*"/active_border = "rgba(cba6f7FF)"/' $colors
-
-        sed -i -E 's/inactive_border = ".*"/inactive_border = "rgba(cba6f733)"/' $colors
-
-        sed -i -E 's/background_color = ".*"/background_color = "rgba(1e1e2eFF)"/' $colors
-
-        sed -i -E 's/border_color = ".*"/border_color = "rgba(cba6f7AA) rgba(cba6f777)"/' $colors
+        # colors.lua -> LILA
+        sed -i -E 's/(active_border[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(cba6f7FF)\2/' $colors
+        sed -i -E 's/(inactive_border[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(cba6f733)\2/' $colors
+        sed -i -E 's/(background_color[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(1e1e2eFF)\2/' $colors
+        sed -i -E 's/(border_color[[:space:]]*=[[:space:]]*")[^"]*(")/\1rgba(cba6f7AA) rgba(cba6f777)\2/' $colors
 
         printf '%s\n' '-- BORDER_VISIBLE_ON' >>$general
 
